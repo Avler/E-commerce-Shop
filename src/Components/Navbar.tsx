bba8df1495572c16deb393005adb94bb74bdd7b2
Kamil Sikora <avler455@gmail.com>
2023-04-14 17:08:47 +0200

    ver-00

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,10 @@
+ 
+ const Navbar = () => {
+    return(
+        <section className="navbar-conteiner">
+
+        </section>
+    )
+ }
+
+ export default Navbar
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return(
+        <div className="home-conteiner">
+
+        </div>
+    )
+}
+
+export default Home
