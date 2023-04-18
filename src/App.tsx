23241f1c7c80447d4a819745f64e7483fc6079d7
Kamil Sikora <avler455@gmail.com>
2023-04-18 22:20:33 +0200

    ver-03

--- src/App.tsx
+++ src/App.tsx
@@ -7,29 +7,39 @@ const supabaseUrl = "https://ejvptagpazmojxlvmjqa.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqdnB0YWdwYXptb2p4bHZtanFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODE0MDY5MjIsImV4cCI6MTk5Njk4MjkyMn0.0nFGE0d_fZhmG4fwMs2k9UoLd5ySAPpZI55ZWPDd1Dc"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+interface Products {
+  id: Number;
+  Category: Text;
+  For: Text;
+  Item: Text;
+  Name: Text;
+  Prize: Number;
+  img: Text;
+}
+
 const App = () => {
 
- const [dataProducts , setDataProducts] = useState([])
+ const [dataProducts , setDataProducts] = useState<Products[]>([])
   
   useEffect( () => {
     async function fetchData() {
       const {data , error} = await supabase
-      .from("Product-For")
-      .select("* ")
+      .from("Products")
+      .select("*")
       if (error) {
         console.log(error)
       } else {
-        setDataProducts(data)
+        setDataProducts(data as Products[])
       }
     }
     fetchData()
   }, []) 
 
-  console.log(dataProducts)
+ 
 
   return (
     <>
-      <Home />
+      <Home data = {dataProducts}/>
     </>
   )
 }
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,26 @@
 import Navbar from "../Components/Navbar/Navbar"
 import SectionMan from "../Components/Section-Man/Section-man"
 
-const Home = () => {
+interface Products {
+    id: Number;
+    Category: Text;
+    For: Text;
+    Item: Text;
+    Name: Text;
+    Prize: Number;
+    img: Text;
+  }
+
+interface HomeProducts {
+    data: Products[];
+}
+
+const Home = (props: HomeProducts) => {
+
+    let data = props.data
+    let dataMan = data.filter(item => item.For === "Man")
+    console.log(dataMan)
+
     return(
         <div className="home-conteiner">
             <Navbar />
