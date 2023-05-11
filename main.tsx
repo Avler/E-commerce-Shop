697b8b6e6a93dab824196b3cfc2de4963eb92df8
Kamil Sikora <avler455@gmail.com>
2023-05-11 20:23:53 +0200

    ver-41

--- main.tsx
+++ main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './src/App'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import  productReducer  from './redux/features/productSlice'
+import  productReducer  from './src/redux/features/productSlice'
 import {configureStore} from "@reduxjs/toolkit"
 
 
